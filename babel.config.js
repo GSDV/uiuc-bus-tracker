32b9083a52a2df86a9fcf494ae277086df425640
GSDV <sdvgab@gmail.com>
2024-01-22 09:57:44 -0600

    Started bus API

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+        'expo-router/babel',
+        ["module-resolver", {
+            "alias": {
+                "@components": "./src/components",
+                "@screens": "./src/screens",
+                "@assets": "./src/assets",
+                "@util": "./src/util"
+            },
+            "extensions": [
+                ".js",
+                ".jsx",
+                ".ts",
+                ".tsx",
+            ]
+        }]
+    ]
   };
 };
