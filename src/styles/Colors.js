eb765348f3c608763dd9d7b18c37b143c4e9bcb0
GSDV <sdvgab@gmail.com>
2024-01-29 14:39:06 -0600

    Defined color scheme

---
+++ src/styles/Colors.js
@@ -0,0 +1,10 @@
+const colorSelection = {
+    bluePrimary: '#0f2952',
+    blueSecondary: '#133770',
+    orangePrimary: '#db5204',
+    orangeSecondary: '#f56c1d',
+    whiteText: '#ebebeb',
+    blackText: '#171717' 
+}
+
+export default colorSelection;
